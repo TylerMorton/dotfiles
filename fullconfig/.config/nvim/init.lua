ba7becb8109f2ea200a6c31ac01c375a9f948b73
Tyler Morton <tbmorton@ucsc.edu>
2024-09-05 22:06:50 -0700

    Initial commit

---
+++ fullconfig/.config/nvim/init.lua
@@ -0,0 +1,59 @@
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.encoding ="utf-8"
+vim.opt.fileformat ="unix"
+
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.tabstop = 8
+vim.opt.expandtab = false
+vim.opt.vb = true
+vim.opt.colorcolumn ='80'
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
+
+
+vim.opt.mouse = 'a'
+
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'kk', '<Esc>')
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	{ "itchyny/lightline.vim", lazy = false,
+		config = function()
+			vim.g.lightline = {
+				colorscheme = "darcula"
+			}
+		end
+	},
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+})
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
+
